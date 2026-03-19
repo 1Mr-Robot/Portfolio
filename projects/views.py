58b7333c58c10adf8370d8691755afe45caf9214
Uziel Flores <uzielo8017518@gmail.com>
2026-03-19 16:01:01 -0600

    Funcionalidad a la seccion de otros proyectos en project.html y cambio de nombre de dominio

--- projects/views.py
+++ projects/views.py
@@ -9,5 +9,6 @@ def projects(request):
 def project(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug, active=True)
     projects = Project.objects.filter(active=True)
+    other_projects = Project.objects.filter(active=True).exclude(id=project.id).order_by('?')
     social_users_in_hero = SocialUser.objects.filter(active=True, is_in_hero=True)
-    return render(request, 'projects/project.html', {'project': project, 'projects': projects, 'social_users_in_hero': social_users_in_hero})
+    return render(request, 'projects/project.html', {'project': project, 'projects': projects, 'other_projects': other_projects, 'social_users_in_hero': social_users_in_hero})
