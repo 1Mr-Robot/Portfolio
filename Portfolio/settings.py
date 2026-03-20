d2c5f2ebe8825a9fc5f3dd5cabf20d7047ff5040
Uziel Flores <uzielo8017518@gmail.com>
2026-03-20 00:28:07 -0600

    Incorporación de django cotton al proyecto y su implementación con head.html

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+    'django_cotton',
     'django_ckeditor_5',
     'adminsortable2',
     'core',
