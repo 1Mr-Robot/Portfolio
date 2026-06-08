c1bb10f90b9ca8373e964192a1bb3f09f396e42f
Uziel Flores <uzielo8017518@gmail.com>
2026-06-08 14:21:44 -0600

    Actualización de nginx para solucionar la vulnerabilidad CVE-2026-9256 y cambios en la seccion de certificados

--- core/models.py
+++ core/models.py
@@ -11,7 +11,7 @@ class Certificate(models.Model):
 
     title = models.CharField(max_length=100, verbose_name="Título")
     institution = models.CharField(max_length=100, verbose_name="Institución")
-    url = models.URLField(max_length=255, blank=True, verbose_name="URL del certificado")
+    url = models.URLField(max_length=255, verbose_name="URL del certificado")
     start_date = models.DateField(verbose_name="Fecha de inicio")
     end_date = models.DateField(blank=True, null=True, verbose_name="Fecha de fin")
     type = models.CharField(max_length=20, choices=TYPES, verbose_name="Tipo de certificado")
